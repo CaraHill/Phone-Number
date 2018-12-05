2e83835d664e673959b56f542468ab23bd6dbee4
Cara Hill <cara@rabidtech.co.nz>
2018-12-05 15:55:37 +1300

    All tests passing.

--- phone-number.js
+++ phone-number.js
@@ -6,7 +6,7 @@ class PhoneNumber {
   number() {
     let number = this.phoneNumber.replace(/\D/g, "").replace(/^1/, "");
 
-    if (number.length < 10 || number.length > 10) {
+    if (number.length < 10 || number.length > 10 || number.charAt(0) === "0" || number.charAt(0) === "1" || number.charAt(3) === "0" || number.charAt(3) === "1") {
       return null
     }
 
